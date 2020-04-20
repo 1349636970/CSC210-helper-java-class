02ccb6c986de4d7a3d40716deb0783e9e02a92e2
1349636970 <a1349636970@gmail.com>
2020-04-20 16:35:31 -0700

    Update test.java

--- test.java
+++ test.java
@@ -1,13 +1,13 @@
-//package app;
+package app;
 
 public class test {
     public static void main(String[] args) {
-        // Helper myHelper = new Helper();
+        Helper myHelper = new Helper();
         // Ask user for input int
         // If you want the user enter the number only greater than 0
-        // System.out.print("Ask user for input int: ");
-        // int inputInt = myHelper.inputInt("errorMessage", 0);
-        // System.out.println(inputInt);
+        System.out.print("Ask user for input int: ");
+        int inputInt = myHelper.inputInt("errorMessage", 0);
+        System.out.println(inputInt);
         /**
          * Output: Ask user for input int: a 
          * errorMessage 
@@ -20,9 +20,9 @@ public class test {
 
 
          //If you want the user enter the number between 1 and 10
-        // System.out.print("Ask user for input int: ");
-        // int inputInt = myHelper.inputInt("errorMessage", 1,10);
-        // System.out.println(inputInt);
+        System.out.print("Ask user for input int: ");
+        int inputIntRange = myHelper.inputInt("errorMessage", 1,10);
+        System.out.println(inputIntRange);
         /* 
         Output: Ask user for input int: a
         errorMessage
@@ -38,12 +38,12 @@ public class test {
         //Same usage for ask user double.
 
         //If you want to get the index of an array
-        // String[] myArray = {"a","b","c","d"};
-        // int index = myHelper.getArrayIndex(myArray, "b", 0);
-        // System.out.println(index);
+        String[] myArray = {"a","b","c","d"};
+        int index = myHelper.getArrayIndex(myArray, "b", 0);
+        System.out.println(index);
         //Output:1
-        // int notExistIndex = myHelper.getArrayIndex(myArray, "3", 0);
-        // System.out.println(notExistIndex);
+        int notExistIndex = myHelper.getArrayIndex(myArray, "3", 0);
+        System.out.println(notExistIndex);
         //Output:-1
     }
 }
