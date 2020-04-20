fb695221953e4fb614da79ce75e383f9a69c0a09
1349636970 <a1349636970@gmail.com>
2020-04-20 16:31:16 -0700

    Add files via upload

---
+++ Helper.java
@@ -0,0 +1,204 @@
+//package app;
+
+import java.util.Scanner;
+
+/**
+ * This helper class provide the Scanner input with validation and get array index.
+ * The validation process I will use while try catch
+ * The most of the code contain duplicate content, if you see this line, I will very grateful for you can make this class more beautiful
+ */
+
+public class Helper {
+    private Scanner input = new Scanner(System.in);
+
+
+
+    /**
+     * This method will ask user for a String and catch all the error message.
+     * If there is a error message, the program will print it out your customize error message and ask the user enter again
+     * The try catch syntax will run the code that you want to try to test if there is error, if there is, the catch syntax will catch it and handle it.
+     * @param errorMessage
+     * @return String the User input
+     */
+
+    public String inputString(String errorMessage) {
+        boolean validation = false;
+        String result = null;
+        while (!validation) {
+            try {
+                result = input.next();
+                validation = true;
+            } catch (Exception e) {
+                System.out.println(errorMessage);
+                input.next();//Clean the input, or it will goes to infinity loop
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 
+     * @param errorMessage
+     * @param smallerNumber
+     * @param greaterNumber
+     * @return
+     */
+    public int inputInt(String errorMessage,int smallerNumber,int greaterNumber) {
+        boolean validation = false;
+        int result = 0;
+        while (!validation) {
+            try {
+                result = input.nextInt();
+                if (result > greaterNumber || result < smallerNumber) {
+                    System.out.println("Plz enter the number between "+smallerNumber+" and "+greaterNumber);
+                    continue;
+                }
+                validation = true;
+            } catch (Exception e) {
+                System.out.println(errorMessage);
+                input.next();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 
+     * @param errorMessage
+     * @param smallerNumber
+     * @return
+     */
+    public int inputInt(String errorMessage,int smallerNumber) {
+        boolean validation = false;
+        int result = 0;
+        while (!validation) {
+            try {
+                result = input.nextInt();
+                if (result < smallerNumber) {
+                    System.out.println("Plz enter the number greater than "+smallerNumber);
+                    continue;
+                }
+                validation = true;
+            } catch (Exception e) {
+                System.out.println(errorMessage);
+                input.next();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * I used Math.abs(result-greaterNumber) > 0.001 to compare the double number because sometimes they will not equal even they are actually equal. For example, there is a number 79.800000000001. If you try to compare this number to 79.8 the computer will tell you they are not equal. But they are actually equal. The way that the computer storage decimals number is not 100% accurate. I will do. Math.abs(79.8-79.80000000001) < 0.001 to check if they are equal
+     * @param errorMessage
+     * @param smallerNumber
+     * @param greaterNumber
+     * @return
+     */
+    public double inputDouble(String errorMessage,double smallerNumber,double greaterNumber) {
+        boolean validation = false;
+        double result = 0.0;
+        while (!validation) {
+            try {
+                result = input.nextInt();
+                if (Math.abs(result-greaterNumber) > 0.001 || Math.abs(result-smallerNumber) < 0.001) {
+                    System.out.println("Plz enter the number between "+smallerNumber+" and "+greaterNumber);
+                    continue;
+                }
+                validation = true;
+            } catch (Exception e) {
+                System.out.println(errorMessage);
+                input.next();
+            }
+        }
+        return result;
+    }
+
+    public double inputDouble(String errorMessage,double smallerNumber) {
+        boolean validation = false;
+        double result = 0.0;
+        while (!validation) {
+            try {
+                result = input.nextInt();
+                if (Math.abs(result-smallerNumber) < 0.001) {
+                    System.out.println("Plz enter the number greater than "+smallerNumber);
+                    continue;
+                }
+                validation = true;
+            } catch (Exception e) {
+                System.out.println(errorMessage);
+                input.next();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * The alternatives way to do this method is for loop. Such like this
+     * for (int i = 0;i < array.length; i++) {
+     *  if (array[i] == element) {
+     *      break
+     *      }
+     *  
+     * }
+     * I think the for loop + if statement is the while loop because while loop has condition and it is a loop. 
+     * 
+     * @param array
+     * @param element
+     * @param startIndex
+     * @return
+     */
+
+    public int getArrayIndex(int[] array, int element,int startIndex) {
+        int result; 
+        int counter = startIndex;
+        while (array[counter] != element) {
+            counter++;
+            if (counter > array.length-1) {
+                break;
+            }
+        }
+        result = counter;
+        if (result == array.length) {
+            result = -1;
+        }
+        return result;
+    }
+
+    public int getArrayIndex(double[] array, double element,int startIndex) {
+        int result; 
+        int counter = startIndex;
+        while (Math.abs(array[counter]-element) < 0.001) {
+            counter++;
+            if (counter > array.length-1) {
+                break;
+            }
+        }
+        result = counter;
+        if (result == array.length) {
+            result = -1;
+        }
+        return result;
+    }
+
+    public int getArrayIndex(String[] array, String element,int startIndex) {
+        int result; 
+        int counter = startIndex;
+        while (array[counter] != element) {
+            counter++;
+            if (counter > array.length-1) {
+                break;
+            }
+        }
+        result = counter;
+        if (result == array.length) {
+            result = -1;
+        }
+        return result;
+    }
+
+
+
+    public void inputClose() {
+        input.close();
+    }
+}
---
+++ test.java
@@ -0,0 +1,49 @@
+//package app;
+
+public class test {
+    public static void main(String[] args) {
+        // Helper myHelper = new Helper();
+        // Ask user for input int
+        // If you want the user enter the number only greater than 0
+        // System.out.print("Ask user for input int: ");
+        // int inputInt = myHelper.inputInt("errorMessage", 0);
+        // System.out.println(inputInt);
+        /**
+         * Output: Ask user for input int: a 
+         * errorMessage 
+         * -1 
+         * Plz enter the number
+         * greater than 0 
+         * 1 
+         * 1
+         */
+
+
+         //If you want the user enter the number between 1 and 10
+        // System.out.print("Ask user for input int: ");
+        // int inputInt = myHelper.inputInt("errorMessage", 1,10);
+        // System.out.println(inputInt);
+        /* 
+        Output: Ask user for input int: a
+        errorMessage
+        -1
+        Plz enter the number between 1 and 10                                                                                                                                                 a
+        11                                                                                                                                                                                    \
+        Plz enter the number between 1 and 10
+        10
+        10 
+        */
+
+
+        //Same usage for ask user double.
+
+        //If you want to get the index of an array
+        // String[] myArray = {"a","b","c","d"};
+        // int index = myHelper.getArrayIndex(myArray, "b", 0);
+        // System.out.println(index);
+        //Output:1
+        // int notExistIndex = myHelper.getArrayIndex(myArray, "3", 0);
+        // System.out.println(notExistIndex);
+        //Output:-1
+    }
+}
